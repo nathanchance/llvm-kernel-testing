6c067b9f5380af401f07028f0a0b09996fed90a1
Nathan Chancellor <natechancellor@gmail.com>
2020-04-14 11:45:57 -0700

    llvm-kernel-testing: Upgrade to Linux 5.6.6
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- qualify-llvm.sh
+++ qualify-llvm.sh
@@ -87,7 +87,7 @@ function build_llvm_binutils() {
 function dwnld_kernel_src() {
     [[ -n ${LINUX_SRC} ]] && return 0
 
-    LINUX=linux-5.6.4
+    LINUX=linux-5.6.6
     LINUX_SRC=${SRC}/${LINUX}
     LINUX_TARBALL=${LINUX_SRC}.tar.xz
 
