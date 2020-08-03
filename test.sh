dc4e27a21ab5ac15077544fd3d768629237cba11
Nathan Chancellor <natechancellor@gmail.com>
2020-08-03 00:51:21 -0700

    test.sh: Update to Linux 5.8
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- test.sh
+++ test.sh
@@ -119,7 +119,7 @@ function build_llvm_binutils() {
 function dwnld_kernel_src() {
     [[ -n ${LINUX_SRC} ]] && return 0
 
-    LINUX=linux-5.7.12
+    LINUX=linux-5.8
     LINUX_SRC=${SRC}/${LINUX}
     LINUX_TARBALL=${LINUX_SRC}.tar.xz
 
