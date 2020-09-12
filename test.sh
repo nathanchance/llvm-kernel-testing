41b6ced49378c9573ab6bdc6722779cac5f40eb0
Nathan Chancellor <natechancellor@gmail.com>
2020-09-12 09:21:15 -0700

    test.sh: Update to Linux 5.8.9
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- test.sh
+++ test.sh
@@ -121,7 +121,7 @@ function build_llvm_binutils() {
 function dwnld_kernel_src() {
     [[ -n ${LINUX_SRC} ]] && return 0
 
-    LINUX=linux-5.8.8
+    LINUX=linux-5.8.9
     LINUX_SRC=${SRC}/${LINUX}
     LINUX_TARBALL=${LINUX_SRC}.tar.xz
 
