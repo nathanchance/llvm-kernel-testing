94e9d73c1005dd3e45bcb283fb2444c1d04c0edc
Nathan Chancellor <natechancellor@gmail.com>
2020-10-29 15:21:22 -0700

    configs: Update
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- configs/fetch.sh
+++ configs/fetch.sh
@@ -25,8 +25,8 @@ function fetch_debian_config() { (
     TMP_DIR=$(mktemp -d -p "${PWD}")
     cd "${TMP_DIR}" || exit ${?}
 
-    PACK_VER=5.8.0-2
-    KER_VER=5.8.10-1
+    PACK_VER=5.9.0-1
+    KER_VER=5.9.1-1
     case ${1} in
         amd64 | arm64) URL=linux-signed-${1}/linux-image-${PACK_VER}-${1}_${KER_VER}_${1}.deb ;;
         armmp) URL=linux/linux-image-${PACK_VER}-${1}_${KER_VER}_armhf.deb ;;
@@ -44,7 +44,7 @@ function fetch_debian_config() { (
 
 # Fedora
 function fetch_fedora_config() {
-    curl -LSso fedora/"${1:?}".config 'https://git.kernel.org/pub/scm/linux/kernel/git/jwboyer/fedora.git/plain/fedora/configs/kernel-5.8.12-'"${1}"'.config?h=kernel-5.8.12-300.fc33'
+    curl -LSso fedora/"${1:?}".config 'https://git.kernel.org/pub/scm/linux/kernel/git/jwboyer/fedora.git/plain/fedora/configs/kernel-5.9.1-'"${1}"'.config?h=kernel-5.9.1-300.fc33'
 }
 
 # OpenSUSE
