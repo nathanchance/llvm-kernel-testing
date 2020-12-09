2f4303c7b48786fdc5520bc3932b9e8bba1ccb67
Nathan Chancellor <natechancellor@gmail.com>
2020-12-08 20:43:06 -0700

    test.sh: Do not build OpenSUSE's powerpc64le config when depending on COMPAT
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- test.sh
+++ test.sh
@@ -570,21 +570,15 @@ function build_powerpc_kernels() {
     log "ppc64le fedora config${LOG_COMMENT} $(results "${?}")"
 
     # OpenSUSE
-    KLOG=powerpc64le-opensuse
-    setup_config opensuse/ppc64le.config
-    # https://github.com/ClangBuiltLinux/linux/issues/944
-    [[ ${LLVM_VER_CODE} -lt 100001 ]] && scripts_config -d ${CTOD}
     # https://github.com/ClangBuiltLinux/linux/issues/1160
-    if grep -q "depends on PPC32 || COMPAT" "${LINUX_SRC}"/arch/powerpc/platforms/Kconfig.cputype; then
-        OTHER_COTD=${COTD}
-        COTD=CONFIG_COMPAT
-        LOG_COMMENT=" (minus ${OTHER_COTD:+${OTHER_COTD} and }${COTD}"
-        scripts_config -d ${COTD}
-    else
-        unset LOG_COMMENT
+    if ! grep -q "depends on PPC32 || COMPAT" "${LINUX_SRC}"/arch/powerpc/platforms/Kconfig.cputype; then
+        KLOG=powerpc64le-opensuse
+        setup_config opensuse/ppc64le.config
+        # https://github.com/ClangBuiltLinux/linux/issues/944
+        [[ ${LLVM_VER_CODE} -lt 100001 ]] && scripts_config -d ${CTOD}
+        kmake "${KMAKE_ARGS[@]}" "${PPC64LE_ARGS[@]}" olddefconfig all
+        log "ppc64le opensuse config $(results "${?}")"
     fi
-    kmake "${KMAKE_ARGS[@]}" "${PPC64LE_ARGS[@]}" olddefconfig all
-    log "ppc64le opensuse config${LOG_COMMENT} $(results "${?}")"
 }
 
 # Build riscv kernels
