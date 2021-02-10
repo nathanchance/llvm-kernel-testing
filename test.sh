9bfc3b894518b532bc04bbaab506fbc87f550797
Nathan Chancellor <nathan@kernel.org>
2021-02-10 10:08:01 -0700

    test.sh: Update to Linux 5.10.15
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- test.sh
+++ test.sh
@@ -124,7 +124,7 @@ function build_llvm_binutils() {
 function dwnld_kernel_src() {
     [[ -n ${LINUX_SRC} ]] && return 0
 
-    LINUX=linux-5.10.14
+    LINUX=linux-5.10.15
     LINUX_SRC=${SRC}/${LINUX}
     LINUX_TARBALL=${LINUX_SRC}.tar.xz
 
