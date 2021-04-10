48b919748c6ef295ebc7c03b43e2076487adbadd
Nathan Chancellor <nathan@kernel.org>
2021-04-09 18:32:54 -0700

    configs/fetch.sh: Update versions/URLs
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.sh
+++ configs/fetch.sh
@@ -30,8 +30,8 @@ function fetch_debian_config() { (
     TMP_DIR=$(mktemp -d -p "${PWD}")
     cd "${TMP_DIR}" || exit ${?}
 
-    PACK_VER=5.10.0-1
-    KER_VER=5.10.4-1
+    PACK_VER=5.10.0-5
+    KER_VER=5.10.26-1
     case ${1} in
         amd64 | arm64) URL=linux-signed-${1}/linux-image-${PACK_VER}-${1}_${KER_VER}_${1}.deb ;;
         armmp) URL=linux/linux-image-${PACK_VER}-${1}_${KER_VER}_armhf.deb ;;
@@ -50,7 +50,7 @@ function fetch_debian_config() { (
 
 # Fedora
 function fetch_fedora_config() {
-    curl -LSso fedora/"${1:?}".config 'https://git.kernel.org/pub/scm/linux/kernel/git/jwboyer/fedora.git/plain/fedora/configs/kernel-5.10.7-'"${1}"'.config?h=kernel-5.10.7-200.fc33'
+    curl -LSso fedora/"${1:?}".config https://src.fedoraproject.org/rpms/kernel/raw/rawhide/f/kernel-"${1}"-fedora.config
 }
 
 # OpenSUSE
