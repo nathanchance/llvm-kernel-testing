bd4faf13e252b12c52c0c28a3764f4ce914d0024
Nathan Chancellor <nathan@kernel.org>
2021-04-16 13:30:22 -0700

    test.sh: Update to Linux 5.11.15
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- test.sh
+++ test.sh
@@ -124,7 +124,7 @@ function build_llvm_binutils() {
 function dwnld_kernel_src() {
     [[ -n ${LINUX_SRC} ]] && return 0
 
-    LINUX=linux-5.11.11
+    LINUX=linux-5.11.15
     LINUX_SRC=${SRC}/${LINUX}
     LINUX_TARBALL=${LINUX_SRC}.tar.xz
 
