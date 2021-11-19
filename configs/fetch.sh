149679bb38baee81fa6c0e5a190351474eed8534
Nathan Chancellor <nathan@kernel.org>
2021-11-19 15:22:35 -0700

    configs: Update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.sh
+++ configs/fetch.sh
@@ -35,10 +35,10 @@ function fetch_debian_config() { (
     TMP_DIR=$(mktemp -d -p "${PWD}")
     cd "${TMP_DIR}" || exit ${?}
 
-    PACK_VER_SIGNED=5.14.0-trunk
-    KER_VER_SIGNED=5.14.3-1~exp1
-    PACK_VER_UNSIGNED=5.14.0-1
-    KER_VER_UNSIGNED=5.14.6-2
+    PACK_VER_SIGNED=5.15.0-trunk
+    KER_VER_SIGNED=5.15.2-1~exp1
+    PACK_VER_UNSIGNED=5.15.0-trunk
+    KER_VER_UNSIGNED=5.15.2-1~exp1
     case ${1} in
         amd64 | arm64) URL=linux-signed-${1}/linux-image-${PACK_VER_SIGNED}-${1}_${KER_VER_SIGNED}_${1}.deb ;;
         armmp) URL=linux/linux-image-${PACK_VER_UNSIGNED}-${1}_${KER_VER_UNSIGNED}_armhf.deb ;;
