dd102939be617545ab1538825fdf835d246c0a75
Nathan Chancellor <nathan@kernel.org>
2022-06-21 09:03:00 -0700

    configs: Update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.sh
+++ configs/fetch.sh
@@ -35,8 +35,8 @@ function fetch_debian_config() { (
     TMP_DIR=$(mktemp -d -p "${PWD}")
     cd "${TMP_DIR}" || exit ${?}
 
-    PACK_VER_SIGNED=5.18.0-1
-    KER_VER_SIGNED=5.18.2-1
+    PACK_VER_SIGNED=5.18.0-2
+    KER_VER_SIGNED=5.18.5-1
     PACK_VER_UNSIGNED=$PACK_VER_SIGNED
     KER_VER_UNSIGNED=$KER_VER_SIGNED
     case ${1} in
