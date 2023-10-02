9034c3d09ecbc9bd2d181e75527b6d4bdc2db326
Nathan Chancellor <nathan@kernel.org>
2023-10-02 11:19:39 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -38,9 +38,9 @@ for distro in $distros
         case debian
             set tmp_dir (mktemp -d -p $dest)
 
-            for arch in amd64 arm64 armmp i386 powerpc64le # s390x ?
-                set package_version_signed 6.5.0-0
-                set kernel_version_signed 6.5.1-1~exp1
+            for arch in amd64 arm64 armmp i386 powerpc64le s390x
+                set package_version_signed 6.5.0-1
+                set kernel_version_signed 6.5.3-1
                 set package_version_unsigned $package_version_signed
                 set kernel_version_unsigned $kernel_version_signed
 
