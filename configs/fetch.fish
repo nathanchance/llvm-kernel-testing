a28181229228a465a5df6334ed64c6c2d36bb82c
Nathan Chancellor <nathan@kernel.org>
2024-10-01 12:32:01 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -46,7 +46,10 @@ for distro in $distros
                 s390x
 
             for arch in $deb_arches
-                set package_version_signed 6.11-rc5
+                set package_version_signed 6.11
+                if test $arch = i386
+                    set package_version_signed 6.11-rc5 # no 6.11 regular for i386?
+                end
                 set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1 # $package_version_signed-1
                 set package_version_unsigned $package_version_signed
                 set kernel_version_unsigned $kernel_version_signed
