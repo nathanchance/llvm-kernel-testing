fab7864b9d898c0d501e55ab8ac50500931a3e89
Nathan Chancellor <nathan@kernel.org>
2025-05-26 09:13:26 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -50,11 +50,9 @@ for distro in $distros
                 s390x
 
             for arch in $deb_arches
-                set package_version_signed 6.14
+                set package_version_signed 6.15-rc7
                 if string match -qr -- -rc $package_version_signed
                     set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
-                else if test "$package_version_signed" = 6.14
-                    set kernel_version_signed $package_version_signed.6-1~exp1
                 else
                     set kernel_version_signed $package_version_signed-1
                 end
