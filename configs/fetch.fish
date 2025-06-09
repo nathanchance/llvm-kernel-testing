454e471d6e5fab78d90b3d0678bb293b0140b927
Nathan Chancellor <nathan@kernel.org>
2025-06-09 10:49:06 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -51,8 +51,10 @@ for distro in $distros
 
             for arch in $deb_arches
                 set package_version_signed 6.15
-                if string match -qr -- -rc $package_version_signed; or test "$package_version_signed" = 6.15
+                if string match -qr -- -rc $package_version_signed
                     set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
+                else if test "$package_version_signed" = 6.15
+                    set kernel_version_signed $package_version_signed.1-1~exp1
                 else
                     set kernel_version_signed $package_version_signed-1
                 end
