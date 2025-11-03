fdf1ffec9c0fcffbc50fa44cbd05d0b933c68c74
Nathan Chancellor <nathan@kernel.org>
2025-11-03 11:40:21 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -60,8 +60,8 @@ cfg.write_text(''.join(cfg_parts), encoding='utf-8')"
                 s390x
 
             for arch in $deb_arches
-                set package_version_signed 6.17.5
-                if string match -qr -- -rc $package_version_signed; or test "$package_version_signed" = 6.17.5
+                set package_version_signed 6.17.6
+                if string match -qr -- -rc $package_version_signed
                     set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
                 else
                     set kernel_version_signed $package_version_signed-1
