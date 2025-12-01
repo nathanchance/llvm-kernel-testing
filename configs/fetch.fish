4f24e10c72e0a20ba348a22446f6a7d55d0a00b3
Nathan Chancellor <nathan@kernel.org>
2025-12-01 11:40:39 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -60,7 +60,7 @@ cfg.write_text(''.join(cfg_parts), encoding='utf-8')"
                 s390x
 
             for arch in $deb_arches
-                set package_version_signed 6.18-rc6
+                set package_version_signed 6.18-rc7
                 if string match -qr -- -rc $package_version_signed
                     set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
                     set package_version_signed (string split -f 1 - $package_version_signed)
