d5757fcc14c7208c5e255fa9a9ea26abf76a54d4
Nathan Chancellor <nathan@kernel.org>
2026-02-23 10:49:47 -0700

    configs: Weekly update
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/fetch.fish
+++ configs/fetch.fish
@@ -53,8 +53,12 @@ for distro in $distros
                 set package_version_signed 6.19
                 if string match -qr -- -rc $package_version_signed; or test $package_version_signed = 6.19
                     set pkgbase linux-base
-                    set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
-                    set package_version_signed (string split -f 1 - $package_version_signed)
+                    if test $package_version_signed = 6.19
+                        set kernel_version_signed (string replace - '~' $package_version_signed).2-1~exp1
+                    else
+                        set kernel_version_signed (string replace - '~' $package_version_signed)-1~exp1
+                        set package_version_signed (string split -f 1 - $package_version_signed)
+                    end
                 else
                     set pkgbase linux-image
                     set kernel_version_signed $package_version_signed-1
