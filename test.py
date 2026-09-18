2792617acea47e4b72c66d10d78092193157f81b
Nathan Chancellor <nathan@kernel.org>
2026-09-18 00:11:51 -0700

    fixup! Rewrite #3 :^)
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+
+from lkt.env import EnvInfo
+from lkt.source import LinuxSourceTree
+from lkt.x86_64 import X8664Matrix
+
+
+def main():
+    env_info = EnvInfo()
+    lst = LinuxSourceTree(Path(os.getenv('CBL_SRC_D'), 'linux-next'))
+
+    matrix = X8664Matrix(lst=lst, env_info=env_info, targets=['def'])
+    print(matrix)
+    print(matrix.jobs)
+
+
+if __name__ == '__main__':
+    main()
