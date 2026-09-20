c585acd957201e064f8831d95c1024d2d39f38e7
Nathan Chancellor <nathan@kernel.org>
2026-09-19 22:40:13 -0700

    fixup! Rewrite #3 :^)
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build.py
+++ build.py
@@ -36,7 +36,8 @@ EXPERIMENTAL_ARCHITECTURES = []
 def parse_arguments():
     parser = ArgumentParser(description='Build a set of Linux kernels with LLVM')
 
-    parser.add_argument(
+    build_options_group = parser.add_argument_group('options to control aspects of build')
+    build_options_group.add_argument(
         '-a',
         '--architectures',
         choices=[*SUPPORTED_ARCHITECTURES, *EXPERIMENTAL_ARCHITECTURES],
@@ -45,51 +46,17 @@ def parse_arguments():
         nargs='+',
         help='Architectures to build for (default: %(default)s).',
     )
-    parser.add_argument(
-        '-b',
-        '--build-folder',
-        type=str,
-        help="Path to build folder (default: 'build' folder in Linux kernel source folder).",
-    )
-    parser.add_argument(
-        '--binutils-prefix',
-        type=str,
-        help="Path to binutils installation (parent of 'bin' folder, default: Use binutils from PATH).",
-    )
-    parser.add_argument(
-        '--boot-utils-folder',
-        type=str,
-        help='Path to boot-utils folder (default: vendored boot-utils).',
-    )
-    parser.add_argument(
-        '-l',
-        '--linux-folder',
-        required=True,
-        type=str,
-        help='Path to Linux source folder (required).',
-    )
-    parser.add_argument(
-        '--llvm-prefix',
-        type=str,
-        help="Path to LLVM installation (parent of 'bin' folder, default: Use LLVM from PATH).",
-    )
-    parser.add_argument(
-        '-o',
-        '--output-folder',
-        type=str,
-        help='Folder to store output files in (default: %(default)s).',
-    )
-    parser.add_argument(
+    build_options_group.add_argument(
         '--only-test-boot',
         action='store_true',
         help='Only build configs that can be booted in QEMU and only build kernel images (no modules)',
     )
-    parser.add_argument(
+    build_options_group.add_argument(
         '--save-objects',
         action='store_true',
         help='Save object files (default: Remove build folder).',
     )
-    parser.add_argument(
+    build_options_group.add_argument(
         '-t',
         '--targets-to-build',
         choices=SUPPORTED_TARGETS,
@@ -98,17 +65,55 @@ def parse_arguments():
         nargs='+',
         help='Testing targets to build (default: %(default)s).',
     )
-    parser.add_argument(
-        '--tc-prefix',
-        type=str,
-        help="Path to toolchain installation (parent of 'bin' folder, default: Use toolchain from PATH).",
-    )
-    parser.add_argument(
+    build_options_group.add_argument(
         '--use-ccache',
         action='store_true',
         help='Use ccache for building (default: Do not use ccache).',
     )
-    parser.add_argument(
+
+    folders_group = parser.add_argument_group('options to customize folders')
+    folders_group.add_argument(
+        '-b',
+        '--build-folder',
+        type=str,
+        help="Path to build folder (default: 'build' folder in Linux kernel source folder).",
+    )
+    folders_group.add_argument(
+        '--boot-utils-folder',
+        type=str,
+        help='Path to boot-utils folder (default: vendored boot-utils).',
+    )
+    folders_group.add_argument(
+        '-l',
+        '--linux-folder',
+        required=True,
+        type=str,
+        help='Path to Linux source folder (required).',
+    )
+    folders_group.add_argument(
+        '-o',
+        '--output-folder',
+        type=str,
+        help='Folder to store output files in (default: %(default)s).',
+    )
+
+    prefix_group = parser.add_argument_group('options to control PATH customization')
+    prefix_group.add_argument(
+        '--binutils-prefix',
+        type=str,
+        help="Path to binutils installation (parent of 'bin' folder, default: Use binutils from PATH).",
+    )
+    prefix_group.add_argument(
+        '--llvm-prefix',
+        type=str,
+        help="Path to LLVM installation (parent of 'bin' folder, default: Use LLVM from PATH).",
+    )
+    prefix_group.add_argument(
+        '--tc-prefix',
+        type=str,
+        help="Path to toolchain installation (parent of 'bin' folder, default: Use toolchain from PATH).",
+    )
+    prefix_group.add_argument(
         '--qemu-prefix',
         type=str,
         help="Path to QEMU installation (parent of 'bin' folder, default: Use QEMU from PATH).",
